8599426c3a5af38c3427be595258358bef2348c4
vaakash0001 <76882797+vaakash0001@users.noreply.github.com>
2024-05-14 07:11:21 +0000

    2nd commit

--- 01_Basics/test1.js
+++ 01_Basics/test1.js
@@ -1,1 +1,12 @@
+
+"use strict" // use all js code as newer version
 console.log("Helooww!!!")
+x = 100
+y = "hello"
+//console.table([typeof(x), typeof(y)])
+{
+    x = 200
+}
+
+console.log(x)
+
---
+++ 01_Basics/test2.js
@@ -0,0 +1,20 @@
+"use strict"; // use all js code as newer version
+
+//alert(3+3) // do not use it bcz we are using nodejs, not browser. We have different module to use this in nodejs
+
+console.log("hi")
+
+//data types
+
+let name = "Aakash";
+let age = 18;
+let isLoggedIn = false;
+
+//number => 2^53
+// Bigint 
+//string
+// null -> standalone value
+// undefined
+// Symbol -> unique
+
+// obbject
